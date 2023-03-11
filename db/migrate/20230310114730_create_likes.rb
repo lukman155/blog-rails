df0f7f5f9706074963ab748ec9a006f888caa55d
Lukman Abdulkarim <109611523+lukman155@users.noreply.github.com>
2023-03-11 04:54:05 +0100

    Fix: Code Review Errors

--- db/migrate/20230310114730_create_likes.rb
+++ db/migrate/20230310114730_create_likes.rb
@@ -3,6 +3,7 @@ class CreateLikes < ActiveRecord::Migration[7.0]
     create_table :likes do |t|
       t.integer :authorid
       t.integer :postid
+      t.references :author, foreign_key: { to_table: 'users' }
 
       t.timestamps
     end

d8c6b7a545c4803c2188849ce49a9d887a0e7fe6
Lukman Abdulkarim <109611523+lukman155@users.noreply.github.com>
2023-03-14 11:44:49 +0100

    Fix: Controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get '/users/:user_id/posts(.:format)' to 'posts#index'
-  get '/users/:user_id/posts/:id(.:format)' to 'posts#show'
-  get '/users(.:format)' to 'users#index'
-  get '/users/:id(.:format)' to 'users#show'
+  get '/users/:user_id/posts(.:format)', to: 'posts#index'
+  get '/users/:user_id/posts/:id(.:format)', to: 'posts#show'
+  get '/users(.:format)', to: 'users#index'
+  get '/users/:id(.:format)', to: 'users#show'
   # Defines the root path route ("/")
   # root "articles#index"
 end

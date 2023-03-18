6b7eae6ba9eb8e0a752554b038e36b4376659092
Lukman Abdulkarim <109611523+lukman155@users.noreply.github.com>
2023-03-18 17:19:55 +0100

    Add: Form

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,16 @@
+class CommentsController < ApplicationController
+  def create
+    @comment = Comment.new(
+      text: params[:text],
+      post_id: params[:post_id]
+    )
+    @comment.user_id = current_user.id
+    if @comment.save
+      flash[:notice] = 'Comment was successfully created.'
+      redirect_to user_post_path(params[:user_id], params[:post_id])
+    else
+      flash.now[:alert] = 'Comment was not created.'
+      render 'posts/show'
+    end
+  end
+end
--- app/models/like.rb
+++ app/models/like.rb
@@ -7,7 +7,7 @@ class Like < ApplicationRecord
 
   def update_like_counts
     post = Post.find(post_id)
-    post.like_count = Like.where(post_id: post.id).count
+    post.likescounter = Like.where(post_id: post.id).count
     post.save
   end
 end
